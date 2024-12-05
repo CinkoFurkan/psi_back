5dcdaf83ac02c02acda8f23ac9c9157f8e6b138c
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-06 00:56:19 +0300

    Onbir

--- psinous/urls.py
+++ psinous/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', include("psinous_app.urls")),
     path('admin/', admin.site.urls),
-    path("psinous_app/", include("psinous_app.urls"))
+
 ]
 
 if settings.DEBUG:
