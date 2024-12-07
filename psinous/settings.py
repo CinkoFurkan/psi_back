993198a713dec30e274ddb3fe153c40845dff936
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-07 13:30:13 +0300

    yirimi

--- psinous/settings.py
+++ psinous/settings.py
@@ -37,7 +37,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["psi-back.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["psi-back.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
