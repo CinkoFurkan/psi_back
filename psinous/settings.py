07930a68b85c0f28f65de5305f45070713073599
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-23 09:04:24 +0300

    host

--- psinous/settings.py
+++ psinous/settings.py
@@ -166,10 +166,10 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["https://psinous.onrender.com",
-    "http://localhost:3000",
+CORS_ALLOWED_ORIGINS = [
     "https://www.psinous.org",
-      
+    "https://psinous.onrender.com",
+    "http://localhost:3000",
 ]
 
 JAZZMIN_SETTINGS = {
