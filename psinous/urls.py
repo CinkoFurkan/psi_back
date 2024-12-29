9e624ddfcf7b6fbb0b03bf058bddbcf61e083161
FurkanÇinko <148985948+CinkoFurkan@users.noreply.github.com>
2024-12-29 23:42:15 +0300

    a

--- psinous/urls.py
+++ psinous/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+admin_url = os.getenv("ADMIN_PAGE")  
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(admin_url, admin.site.urls), 
     path("psinous_app/", include("psinous_app.urls")),
 ]
 
